0ca9eb6e96b0cff39826c7832ab59750bca684e5
Agung Sepruloh <agungsepruloh1996@gmail.com>
2024-01-01 08:25:32 +0700

    [IMP] base.order.line: Autofill product barcode when product id is entered

--- base_product_barcode/models/base_order_line.py
+++ base_product_barcode/models/base_order_line.py
@@ -12,9 +12,17 @@ class BaseOrderLine(models.AbstractModel):
             product = self.env['product.product'].search([('barcode', '=', self.product_barcode)])
             self.product_id = product.id
 
+    @api.onchange('product_id')
+    def _onchange_product_id(self):
+        if self.product_id:
+            self.product_barcode = self.product_id.barcode
+
     @api.model
     def create(self, vals):
         if vals.get('product_barcode') and not vals.get('product_id'):
             product = self.env['product.product'].search([('barcode', '=', vals.get('product_barcode'))])
             vals['product_id'] = product.id
+        if vals.get('product_id') and not vals.get('product_barcode'):
+            product = self.env['product.product'].browse(vals.get('product_id'))
+            vals['product_barcode'] = product.barcode
         return super(BaseOrderLine, self).create(vals)
